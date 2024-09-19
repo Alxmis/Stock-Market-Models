fabff31cd8e8948a722a601d07398eba58a601da
aellieme <elyasup@yandex.ru>
2024-09-19 16:32:40 +0300

    Showing a graph

--- show9.py
+++ show9.py
@@ -1,6 +1,17 @@
-plt.plot(day, array, linestyle='-')
-plt.xlabel('День')
-plt.ylabel('Стоимость')
-plt.title('Динамика стоимости акции',tiker)
-end = time.time()
-plt.show()
+import matplotlib.pyplot as plt
+
+
+def showing_graph_9(stock_Name, day, stock_returns):
+    plt.plot(day, stock_returns, linestyle='-')
+    plt.xlabel('День')
+    plt.ylabel('Стоимость')
+    plt.title(f'Динамика стоимости акции {stock_Name}')
+    plt.show()
+
+
+stock_Name = 'Лукойл'
+stock_returns = log_returns[stock_Name] 
+
+day = list(range(360))  
+
+showing_graph_9(stock_Name, day, stock_returns)
